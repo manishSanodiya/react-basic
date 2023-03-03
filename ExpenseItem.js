14135d5a1514604e5b3bee31212ad4eaf13b0b16
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-03-03 10:19:18 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,1 +1,9 @@
-
+function ExpenseItem(){
+    return <div>
+        <h2>Expense Items!</h2>
+    <p>food rs 10</p>
+    <p>petrol rs 100</p>
+    <p>movie rs 200</p>
+        </div>
+}
+export default ExpenseItem;
